09917dd9cb4ded77d3739800761a14671d92ed8c
unknown <shireenkk28@gmail.com>
2021-05-27 21:00:20 +0530

    [shireen]Added the login component and html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './Pages/registration/registration.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegistrationComponent }
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
